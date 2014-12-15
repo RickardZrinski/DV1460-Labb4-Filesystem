9f048928d86b1abd2366d78ff4a5050caf06bf6a
Rickard Zrinski <ricco3@hotmail.com>
2014-12-15 20:27:49 +0100

    Reverse changes to TestShell

--- Filesystem/src/TestShell.java
+++ Filesystem/src/TestShell.java
@@ -2,15 +2,9 @@ public class TestShell
 {
   public static void main(String[] args)
     {
-      //MemoryBlockDevice BlockTest=new MemoryBlockDevice();
-      //Filesystem FS=new Filesystem(BlockTest);
-      //Shell Bash=new Shell(FS,null);
-      //Bash.start();
-
-      FileTree k = new FileTree();
-
-      k.addEntrys();
-      k.printall();
-
+      MemoryBlockDevice BlockTest=new MemoryBlockDevice();
+      Filesystem FS=new Filesystem(BlockTest);
+      Shell Bash=new Shell(FS,null);
+      Bash.start();
     }
 }
