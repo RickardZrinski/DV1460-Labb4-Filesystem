07d481f6049fb8589c6f79573561a098584e8739
Rickard Zrinski <ricco3@hotmail.com>
2014-12-15 17:45:19 +0100

    Add basic Entry class

---
+++ Filesystem/src/Entry.java
@@ -0,0 +1,67 @@
+/**
+ * Created by Rickard on 2014-12-15.
+ */
+public class Entry
+{
+    boolean isDirectory;
+    String name;
+    int size;
+    String creationDate;
+    byte[] data;
+
+    Entry(String name, boolean isDirectory)
+    {
+        this.name = name;
+        this.isDirectory = isDirectory;
+    }
+
+    public boolean isDirectory()
+    {
+        return isDirectory;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public String getCreationDate()
+    {
+        return creationDate;
+    }
+
+    public byte[] getData()
+    {
+        return data;
+    }
+
+    public void setDirectory(boolean isDirectory)
+    {
+        this.isDirectory = isDirectory;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+
+    public void setCreationDate(String creationDate)
+    {
+        this.creationDate = creationDate;
+    }
+
+    public void setData(byte[] data)
+    {
+        this.data = data;
+    }
+}
