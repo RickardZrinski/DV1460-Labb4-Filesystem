fe49819feee860a708fbc814ff2d9815fbe4de15
PolHaghverdian <pol_r@hotmail.com>
2014-12-17 17:48:03 +0100

    no message

--- Filesystem/src/MemoryBlockDevice.java
+++ Filesystem/src/MemoryBlockDevice.java
@@ -1,58 +1,77 @@
 public class MemoryBlockDevice extends BlockDevice
 {
   byte[][] m_abContents=new byte[250][512];
-  //@TODO make sorted list to make a nicer implementation incase a file is deleted, so there are no empty slots
   int nextAvailableIndex;
 
 
 
   public MemoryBlockDevice()
   {
-    nextAvailableIndex = 0;
+    for (int f = 0; f<250; f++)
+    {
+      m_abContents[i][0] = -1;
+    }
+
+    nextAvailableIndex = findAvailableIndex();
   }
   public int writeBlock(int p_nBlockNr,byte[] p_abContents)
+  {
+    if(p_nBlockNr>249 || p_nBlockNr<0)
     {
-      if(p_nBlockNr>249 || p_nBlockNr<0)
-        {
-          // Block out-of-range
-          return -1;
-        }
-
-      if(p_abContents.length!=512)
-        {
-          // Block size out-of-range
-          return -2;
-        }
-
-      for(int nIndex=0;nIndex<512;nIndex++)
-        {
-          m_abContents[p_nBlockNr][nIndex]=p_abContents[nIndex];
-        }
-      nextAvailableIndex++;
-      
-      return 1;
-      
+      // Block out-of-range
+      return -1;
     }
+
+    if(p_abContents.length!=512)
+    {
+      // Block size out-of-range
+      return -2;
+    }
+
+    for(int nIndex=0;nIndex<512;nIndex++)
+    {
+      m_abContents[p_nBlockNr][nIndex]=p_abContents[nIndex];
+    }
+
+    nextAvailableIndex = findAvailableIndex();
+
+    return 1;
+
+  }
 
   public byte[] readBlock(int p_nBlockNr)
+  {
+    if(p_nBlockNr>249 || p_nBlockNr<0)
     {
-      if(p_nBlockNr>249 || p_nBlockNr<0)
-        {
-          // Block out-of-range
-          return new byte[0];
-        }
-
-      byte[] abBlock=new byte[512];
-
-      for(int nIndex=0;nIndex<512;nIndex++)
-        {
-          abBlock[nIndex]=m_abContents[p_nBlockNr][nIndex];
-        }
-
-      return abBlock; 
+      // Block out-of-range
+      return new byte[0];
     }
 
+    byte[] abBlock=new byte[512];
+
+    for(int nIndex=0;nIndex<512;nIndex++)
+    {
+      abBlock[nIndex]=m_abContents[p_nBlockNr][nIndex];
+    }
+
+    return abBlock;
+  }
+
   public int getNextAvailableIndex() {
+    return nextAvailableIndex;
+  }
+
+  private int findAvailableIndex()
+  {
+
+    for(int i = 0; i<250; i++)
+    {
+      if(m_abContents[i][0] == -1)
+      {
+        nextAvailableIndex = i;
+      }
+    }
+
     return nextAvailableIndex;
   }
 }
